daa5ffced5638ae6462ab952344dfc35a31b963d
barroo8 <bimber888@gmail.com>
2022-04-27 13:09:29 +0300

    2
    2

--- src/Q1/Jellyfish.java
+++ src/Q1/Jellyfish.java
@@ -196,36 +196,7 @@ public class Jellyfish extends Swimmable {
 	}
 	@Override
 	public void run() {
-		drawAnimal(panel.getGraphics());
-		while(true) {
-			try {
-				drawAnimal(panel.getGraphics());
-				sleep(15);
-				if(this.x_front > panel.getWidth()&& x_dir ==1 )
-				{
-					x_dir =-1;
-				}
-				else if (this.x_front < 0 && x_dir ==-1 )
-				{
-					x_dir =1;
-				}
-				if(this.y_front > panel.getHeight() && y_dir ==1 )
-				{
-					y_dir =-1;
-				}
-				else if (this.y_front < 0 && y_dir ==-1 )
-				{
-					y_dir =1;
-				}
-				this.y_front += this.verSpeed*this.y_dir;
-				this.x_front += this.horSpeed*this.x_dir;
-				sleep(15);
-				panel.repaint();
-				sleep(15);
-				//drawAnimal(panel.getGraphics());
-				//this.drawAnimal(g);
-			}catch(InterruptedException e) {}
-		}
+		// TODO Auto-generated method stub
 		
 	}
 	@Override
