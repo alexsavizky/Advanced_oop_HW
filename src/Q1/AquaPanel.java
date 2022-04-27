fe430ee0a64f5e449f6b68845b962a0ddcd9ced3
barroo8 <bimber888@gmail.com>
2022-04-27 21:04:27 +0300

    Update AquaPanel.java

--- src/Q1/AquaPanel.java
+++ src/Q1/AquaPanel.java
@@ -144,7 +144,6 @@ public class AquaPanel extends JPanel implements ActionListener
 				while(itrAnimals.hasNext()) 
 				{
 					s = itrAnimals.next();
-		
 					name = s.getAnimalName();
 					color = s.getColor();
 					size = s.getSize();
